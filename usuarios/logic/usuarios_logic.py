337829f874c05255e22553c7682253b6a30c337f
gorgonzolast <85966440+gorgonzolast@users.noreply.github.com>
2023-03-18 12:43:29 -0500

    Cambio a lógica

--- usuarios/logic/usuarios_logic.py
+++ usuarios/logic/usuarios_logic.py
@@ -1,34 +1,10 @@
 from ..models import Usuario
 
 def get_usuarios():
-    usuarios=Usuario.objects.all()
-    return usuarios
+    queryset = Usuario.objects.all().order_by('-dateTime')[:10]
+    return (queryset)
 
-def get_usuario(var_pk):
-    usuario=Usuario.objects.get(pk=var_pk)
-    return usuario
-
-def update_usuario(sol_pk, new_sol):
-    usuario = get_usuario(sol_pk)
-    usuario.cedula = new_sol["cedula"]
-    usuario.nombre = new_sol["nombre"]
-    usuario.telefono = new_sol["telefono"]
-    usuario.correo = new_sol["correo"]
-    usuario.direccion = new_sol["direccion"]
-    usuario.save()
-    return usuario
-
-
-
-def create_usuario(sol):
-    cliente = Usuario(
-        cedula = sol["cedula"],
-        nombre = sol["nombre"],
-        telefono = sol["telefono"],
-        correo = sol["correo"],
-        direccion = sol["direccion"]
-    )
+def create_usuario(form):
+    cliente = form.save()
     cliente.save()
-    return cliente
-
-
+    return ()
