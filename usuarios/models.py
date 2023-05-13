79e5147a7ebc86f6bf199312cd3da0ba364c7af1
davidvaldh <ds.valderrama@uniandes.edu.co>
2023-05-13 12:19:49 -0500

    campos unicos removidos

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Usuario(models.Model):
-    cedula=models.IntegerField(null=False,blank=False,unique=True)
+    cedula=models.IntegerField(null=False,blank=False,unique=False)
     nombre = models.CharField(max_length = 50, null = False, blank = False)
-    telefono = models.IntegerField(null = False, blank = False, unique = True)
-    correo = models.CharField(max_length = 50, null = False, blank = False, unique = True)
+    telefono = models.IntegerField(null = False, blank = False, unique = False)
+    correo = models.CharField(max_length = 50, null = False, blank = False, unique = False)
     direccion = models.CharField(max_length = 50, null = False, blank = False)
 
 
